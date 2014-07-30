9e0fc81cd056a6c319906e88d7fb0f6bc6460308
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2014-07-30 18:01:56 +0100

    Now exports as SQL file ready to run

--- bibliographyDuplicateCleanup.py
+++ bibliographyDuplicateCleanup.py
@@ -1,9 +1,9 @@
 """ Generate SQL to Transfer Duplicate Bibliography Entries to Single Entry
 
-(csv file) -> (csv file)
+(csv file) -> (sql file)
 
-Returns a CSV file with 1 SQL statement per line in the form:
-UPDATE bibliography SET pubID='1' WHERE pubID='2';
+Returns a SQL file with 1 SQL statement per line in the form:
+UPDATE bibliography SET pubID='1' WHERE pubID='2';
 
 Input CSV rows must have the following attributes:
 value1,value2,value3
@@ -18,23 +18,26 @@ CSV file must be sorted by value1 ascending and then by value2 descending, e.g.
 2,1,0011A0000100112C
 2,,0011A00001001257
 
-Version 1
-Since 28 July 2014
+Version 2
+Since 30 July 2014
 Copyright Mary Chester-Kadwell 2014
 Written in Python 3.4 for the Portable Antiquities Scheme database
 
 """
+
+## Specify your parameters here
+fileIn = 'inputs/test-in-bibliographyDuplicates.csv'
+fileOut = 'outputs/test-out-bibliography.sql'
 
 import csv
 
 update = "UPDATE bibliography "
 setclause = "SET pubID='"
 where = "' WHERE pubID='"
-end = "';"
+end = "';\n"
 
-with open('test-out.csv', 'w', newline='') as csvFileOut:
-    myWriter = csv.writer(csvFileOut, delimiter=',')
-    with open('test-bibliographyDuplicates.csv', newline='') as csvFileIn:
+with open(fileOut, 'w', newline='') as sqlFileOut:
+    with open(fileIn, newline='') as csvFileIn:
         myReader = csv.reader(csvFileIn, delimiter=',')
         changeTo = ''
         changeFrom = ''
@@ -43,4 +46,4 @@ with open('test-out.csv', 'w', newline='') as csvFileOut:
                 changeTo = row[2]
             else:
                 changeFrom = row[2]
-                myWriter.writerow([update + setclause + changeTo + where + changeFrom + end])
+                sqlFileOut.write(update + setclause + changeTo + where + changeFrom + end)
