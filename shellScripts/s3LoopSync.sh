e954512e581a015e7c9b28f2107746cc529ac573
Daniel Pett <dpett@britishmuseum.org>
2015-05-05 23:16:35 +0100

    Added shell script for s3

---
+++ shellScripts/s3LoopSync.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Bash script for backing up image folder on Asgard to AWS S3
+# Needs to be used in conjunction with s3cmd installed on server and configured.
+# Daniel Pett, 5th May 2015
+# To use sh /path/to/script/s3LoopSync.sh
+
+for d in /var/www/beowulf/public_html/images/*/ ; do
+    echo "$d"
+    dir=$(basename "$d")
+    echo "$dir"
+    sudo s3cmd sync $d --skip-existing --preserve --verbose --recursive  --exclude 'zoom/' --exclude 'small/' --exclude 'medium/' --exclude 'display/' s3://findsorguk-images/$dir/
+done;
